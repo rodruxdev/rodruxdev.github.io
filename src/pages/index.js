9471f22db43aee1e43565d296533cc7cdcb5009f
rodrigojgq <rodrigojgq@gmail.com>
2022-04-15 10:48:39 -0400

    ✨[ADD] Font and title in Head

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import AboutSection from '@containers/AboutSection';
 import Footer from '@containers/Footer';
 import Header from '@containers/Header';
 import Hero from '@containers/Hero';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Rodrigo Goitia | RODRUX</title>
+      </Head>
       <Header />
       <Hero />
       <AboutSection />

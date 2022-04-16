ce40a191b0a0d7a202e460e4c749abd7f6aa93ba
rodrigojgq <rodrigojgq@gmail.com>
2022-04-16 16:46:01 -0400

    🔧[FIX] Font weight

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,500;0,600;1,600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,500;1,500&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />

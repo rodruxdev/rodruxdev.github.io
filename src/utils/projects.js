55ffe24c4aae741e2ffb5f424c4a423a5f240704
rodrigojgq <rodrigojgq@gmail.com>
2022-07-01 22:44:23 -0400

    ✨New project information

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,7 +3,7 @@ const PROJECTS = [
     title: 'Rodrux Player',
     description: 'This is a web page to practice animations and mockup with html and css. The page has neuromorphic design and few interactions with buttons.',
     image: 'rodrux-player.png',
-    stack: ['html', 'css', 'js'],
+    stack: ['html', 'css', 'javascript'],
     urlWeb: 'https://rodruxdev.github.io/rodrux-player/',
     urlGithub: 'https://github.com/rodruxdev/rodrux-player',
   },
@@ -12,7 +12,7 @@ const PROJECTS = [
     description:
       "A web app to see cryptocoins and tokens information. The page brings the information from Coingecko's API. The user can search coins by title and see the coin's information, a coin convertor and some related coins to look after. It has simple unit testing with Jest and Testing Library for some components and uses Redux to manage the state.",
     image: 'coin-watcher.png',
-    stack: ['html', 'css', 'js', 'react.js', 'redux', 'jest', 'testing library', 'webpack'],
+    stack: ['html', 'css', 'javascript', 'react.js', 'redux', 'jest', 'testing library', 'webpack'],
     urlWeb: 'https://rodrux-coin-watcher.vercel.app/',
     urlGithub: 'https://github.com/rodruxdev/rodrux-coin-watcher',
   },
@@ -21,7 +21,7 @@ const PROJECTS = [
     description:
       "This web app let you save your buying list, check the items you bought and delete the items. It saves the items list in the browser's local storage . This project uses React.js and its hooks to manage the state.",
     image: 'buying-list.png',
-    stack: ['html', 'css', 'js', 'react.js'],
+    stack: ['html', 'css', 'javascript', 'react.js'],
     urlWeb: 'https://rodruxdev.github.io/buying-list/',
     urlGithub: 'https://github.com/rodruxdev/buying-list',
   },
@@ -30,18 +30,18 @@ const PROJECTS = [
     description:
       "This is a game to decipher some words using Cesar Cipher. This page also let you cipher or decipher your own messages. This page follows the instruction in a project of Laboratoria's Bootcamp.",
     image: 'cesar-cipher-game.png',
-    stack: ['html', 'css', 'js', 'jest'],
+    stack: ['html', 'css', 'javascript', 'jest'],
     urlWeb: 'https://rodruxdev.github.io/cesar-cipher-game/',
     urlGithub: 'https://github.com/rodruxdev/cesar-cipher-game',
   },
   {
-    title: 'Cesar Cipher Game',
+    title: 'Trivia GOTR',
     description:
-      "This is a game to decipher some words using Cesar Cipher. This page also let you cipher or decipher your own messages. This page follows the instruction in a project of Laboratoria's Bootcamp.",
-    image: 'cesar-cipher-game.png',
-    stack: ['html', 'css', 'js', 'jest'],
-    urlWeb: 'https://rodruxdev.github.io/cesar-cipher-game/',
-    urlGithub: 'https://github.com/rodruxdev/cesar-cipher-game',
+      "This is a Trivia game about Game of Thrones and The Lord of the Ring. You have to answer six questions in 30 seconds each. Then you see the wrong and correct answers. If time runs out you have to play again. This page follows the instruction in a project of Laboratoria's Bootcamp.",
+    image: 'trivia-GOTR.png',
+    stack: ['html', 'css', 'javascript', 'jest'],
+    urlWeb: 'https://rodruxdev.github.io/trivia-GOTR/src/choice.html',
+    urlGithub: 'https://github.com/rodruxdev/trivia-GOTR',
   },
 ];
 
